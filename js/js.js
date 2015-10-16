8d28187fc766aa5dda821fd482e0d74932a6c5d9
alizhj <lisahjarpe@gmail.com>
2015-10-16 11:01:32 +0200

    Project

---
+++ js/js.js
@@ -0,0 +1,8 @@
+	/* ######################################## */
+	/* ###### toggle navigation fucntion ###### */
+	/* ######################################## */
+	$(document).ready(function(){
+		$("#toggle_navbar").on('click', function(){
+			$('ol').slideToggle();
+		});
+	});
